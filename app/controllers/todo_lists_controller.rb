0c455bb60bbe2e11c79064d68c941e077fb5e2b4
rafaelrochasilva <rafaelrochasilva@gmail.com>
2014-12-03 09:49:41 -0200

    Add todo list new and create controller

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,3 +1,22 @@
 class TodoListsController < ApplicationController
+	def new
+		@todo_list = TodoList.new
+	end
 
+	def create
+		@todo_list = TodoList.new(todo_params)
+
+		if @todo_list.valid?
+			flash[:notice] = "Todo was successfully created."
+			redirect_to @todo_list
+		else
+			flash[:notice] = "Coudn't create a Todo."
+			render 'new'
+		end
+	end
+
+	private
+	def todo_params
+		params.require(:todo_list).permit(:name, :private_todo, :user_id)
+	end
 end
