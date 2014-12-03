7d0bb8e49d79da004597b7f83aba5a1abdbd154b
rafaelrochasilva <rafaelrochasilva@gmail.com>
2014-12-03 14:25:55 -0200

    Add edit and update actions to todo list

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -30,6 +30,22 @@ class TodoListsController < ApplicationController
 		end
 	end
 
+	def edit
+		@todo_list = TodoList.find(params[:id])
+	end
+
+	def update
+		@todo_list = TodoList.find(params[:id])
+
+		if @todo_list.update(todo_params)
+			flash[:notice] = "Todo was successfully updated."
+			redirect_to @todo_list
+		else
+			flash[:notice] = "Couldn't update a Todo."
+			render 'edit'
+		end
+	end
+
 	private
 	def todo_params
 		params.require(:todo_list).permit(:name, :private_todo, :user_id)
