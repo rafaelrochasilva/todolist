09664ff23ef372b16a11d33319dcbced0040be27
rafaelrochasilva <rafaelrochasilva@gmail.com>
2014-12-03 08:25:24 -0200

    Create a feature test for todo list

---
+++ spec/features/todo_list_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'TodoList management' do
+	let(:user) { FactoryGirl.create(:user) }
+	before { login_as(user, scope: :user) }
+
+	let(:todo_list) { FactoryGirl.build(:todo_list) }
+
+
+	scenario 'adds a new todo list' do
+	  visit root_path
+	  click_link 'New Todo'
+
+	  fill_in 'Name', with: todo_list.name
+
+	  click_button "Add"
+
+
+	end
+
+end
