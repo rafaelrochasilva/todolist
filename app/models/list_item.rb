4e15b7175e29f03f156179e7311c1ac0a7f24e89
rafaelrochasilva <rafaelrochasilva@gmail.com>
2014-12-03 16:48:57 -0200

    Add presence validation for description

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -1,3 +1,4 @@
 class ListItem < ActiveRecord::Base
 	belongs_to :todo_list
+	validates_presence_of :description
 end
--- spec/models/list_item_spec.rb
+++ spec/models/list_item_spec.rb
@@ -2,5 +2,14 @@ require 'rails_helper'
 
 describe ListItem do
 
+	it 'is valid with description and todo list id' do
+		list_item = FactoryGirl.create(:list_item)
+		expect(list_item).to be_valid
+	end
 
+	it 'is invalid without description' do
+		list_item = FactoryGirl.build(:list_item, description: nil)
+		list_item.valid?
+		expect(list_item.errors[:description]).to include "can't be blank"
+	end
 end
