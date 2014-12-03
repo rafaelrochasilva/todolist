699a0d8ae3b5016eacd5199a0222839bfed78f88
rafaelrochasilva <rafaelrochasilva@gmail.com>
2014-12-03 11:16:47 -0200

    Add tests for my todos

--- spec/features/todo_list_spec.rb
+++ spec/features/todo_list_spec.rb
@@ -18,11 +18,21 @@ feature 'TodoList management' do
 	  expect(page).to have_content 'Todo was successfully created.'
 	end
 
-	scenario 'displays all public todo list' do
+	scenario 'displays all public todo lists' do
 	  visit root_path
 	  click_link 'All Todos'
 
 	  expect(page).to have_content "All Public Todos"
+	end
+
+	scenario 'display all private todo lists' do
+		visit root_path
+		click_link "My Todos"
+		expect(page).to have_content "My Private Todos"
+	end
+
+	scenario 'display one todo list' do
+
 	end
 
 end
