1de3294ae3b93f8b659978f592d020030cb94aa7
rafaelrochasilva <rafaelrochasilva@gmail.com>
2014-12-03 11:13:05 -0200

    Fix todo list association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	has_many :todos
+	has_many :todo_lists
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
