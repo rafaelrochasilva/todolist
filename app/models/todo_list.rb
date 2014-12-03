cf6cd016d497519a0bad2404c2ae1d336ae39d9d
rafaelrochasilva <rafaelrochasilva@gmail.com>
2014-12-03 08:26:59 -0200

    Add tests to validate an user

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,4 +1,4 @@
 class TodoList < ActiveRecord::Base
 	belongs_to :user
-	validates_presence_of :name
+	validates_presence_of :name, :user
 end
--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -7,6 +7,12 @@ describe TodoList do
 		expect(todo_list).to be_valid
 	end
 
+	it "is invalid without a user" do
+		todo_list = FactoryGirl.build(:todo_list, user: nil)
+		todo_list.valid?
+		expect(todo_list.errors[:user]).to include("can't be blank")
+	end
+
 	it "is invalid without name" do
 		todo_list = FactoryGirl.build(:todo_list, name: nil)
 		todo_list.valid?
