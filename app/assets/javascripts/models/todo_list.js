5eb156364234e665eb35178fc2c39c4a092150fa
rafaelrochasilva <rafaelrochasilva@gmail.com>
2014-12-04 07:52:45 -0200

    Refactor grab todo ID

--- app/assets/javascripts/models/todo_list.js
+++ app/assets/javascripts/models/todo_list.js
@@ -1,5 +1,6 @@
 var TodoList = {
-	BTN_ADD_ITEM: ".btn-add-item"
+	BTN_ADD_ITEM: ".btn-add-item",
+	BTN_SUBMIT_ITEM: ".btn-submit-item",
 };
 
 TodoList.addItemListener = function(){
@@ -7,9 +8,32 @@ TodoList.addItemListener = function(){
 	$(this.BTN_ADD_ITEM).click(function(e){
 		e.preventDefault();
 
-		var id = $(".title").attr('id');
+		// TodoList.id = $(".title").attr('id');
+		TodoList.grabTodoID();
+
 		TodoView.hideBtnAddItem();
-		TodoView.makeFormItem(id);
+		TodoView.makeFormItem(TodoList.id);
 	});
 };
+
+TodoList.grabTodoID = function(){
+	this.id = $(".title").attr('id');
+};
+
+TodoList.submitItemListener = function(){
+	$(this.BTN_SUBMIT_ITEM).click(function(e){
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: '/todo_lists/:todo_list_id/list_item',
+			dataType: 'json'
+		}).done(function(data){
+			alert("sent");
+		}).fail(function(data){
+			alert("Can't create a new Item");
+		});
+	});
+};
+
+
 
