c3492016497a866ddc75c50cf96e9fba2ff2d7b9
rafaelrochasilva <rafaelrochasilva@gmail.com>
2014-12-04 09:10:12 -0200

    Refactor hide element

--- app/assets/javascripts/views/todo_list_view.js
+++ app/assets/javascripts/views/todo_list_view.js
@@ -1,11 +1,15 @@
 var TodoView = {
 	TODO_FORM: ".new_form",
 	BTN_ADD_ITEM: ".btn-add-item",
-	IMG_LOADER: "http://upload.wikimedia.org/wikipedia/commons/d/de/Ajax-loader.gif"
+	IMG_LOADER: "/assets/ajax_loader.gif"
 };
 
-TodoView.hideBtn = function(btn){
+TodoView.hideElement = function(btn){
 	btn.hide();
+};
+
+TodoView.showElement = function(btn){
+	btn.show();
 };
 
 TodoView.makeFormItem = function(todo_list_id){
@@ -24,11 +28,14 @@ TodoView.addListItem = function(){
 };
 
 TodoView.createInputField = function(){
-	return "<input id='description' name='todo_list_item[description]'></input>";
+	return "<input id='description' name='todo_list_item[description]'>";
 };
 
 TodoView.createSubmitBtn = function(){
 	return "<input class='btn-submit-item' type='submit' value='Add'>";
 };
 
-TodoView.displayLoading = function(){};
+TodoView.displayLoading = function(){
+	var img_loading = "<img src="+this.IMG_LOADER+"/>";
+	$(this.TODO_FORM).append(img_loading);
+};
