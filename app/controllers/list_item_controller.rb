851898a54860824cca628e1e20479699bf059d73
rafaelrochasilva <rafaelrochasilva@gmail.com>
2014-12-04 17:18:41 -0200

    Fix list item destroy

--- app/controllers/list_item_controller.rb
+++ app/controllers/list_item_controller.rb
@@ -1,18 +1,21 @@
 class ListItemController < ApplicationController
+	before_action :authenticate_user!
+
 	respond_to :json
 
 	def create
 		todo_list = TodoList.find(params[:todo_list_id])
-		@list_item = todo_list.list_items.create(list_item_params)
-
-		respond_with(@list_item, :location => nil)
+		if current_user.id == todo_list.user.id
+			@list_item = todo_list.list_items.create(list_item_params)
+		end
+		respond_with(@list_item, location: nil)
 	end
 
 	def destroy
 		@list_item = ListItem.find(params[:id])
 		@list_item.destroy
 
-		respond_with(@list_item, :location => nil)
+		respond_with(@list_item, location: todo_list_path(id: params[:todo_list_id]))
 	end
 
 	private
