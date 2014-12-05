fa8042cf84b5c85473f95f6b0038d13d0283e59e
rafaelrochasilva <rafaelrochasilva@gmail.com>
2014-12-05 18:02:54 -0200

    Add eager to favorite todos including todo_list

--- app/controllers/favorite_todos_controller.rb
+++ app/controllers/favorite_todos_controller.rb
@@ -1,17 +1,15 @@
 class FavoriteTodosController < ApplicationController
 
 	def index
-		@my_favorites = current_user.favorite_todos.all
+		@my_favorites = current_user.favorite_todos.includes(:todo_list).all
 	end
 
 	def favorite
 		@todo_list = TodoList.find(params[:id])
 
 		if params[:type] == "favorite"
-			favorite = current_user.favorite_todos.new(todo_list_id: @todo_list.id)
-			if favorite.save
-				flash[:notice] = "Add #{@todo_list.name} as favorite"
-			end
+			favorite = current_user.favorite_todos.find_or_create_by(todo_list_id: @todo_list.id)
+			flash[:notice] = "Add #{@todo_list.name} as favorite"
 		else
 			favorite = current_user.favorite_todos.find_by(todo_list_id: @todo_list.id)
 			if favorite
