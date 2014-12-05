c7b5bc691f0dc43e58c50c573bebd89d2fe3c6af
rafaelrochasilva <rafaelrochasilva@gmail.com>
2014-12-05 11:50:05 -0200

    Add association with todo and fav

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
 	has_many :todo_lists
+	has_many :favorite_todos
+	has_many :favorites, through: :favorite_todos
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
