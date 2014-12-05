4edeab2ed8d324bd33129008eb5e31b700a20ed9
rafaelrochasilva <rafaelrochasilva@gmail.com>
2014-12-05 11:49:28 -0200

    Add favorite todo model

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,5 +1,8 @@
 class TodoList < ActiveRecord::Base
 	belongs_to :user
 	has_many :list_items
+	has_many :favorite_todos
+	has_many :favorited_by, through: :favorite_todos, source: :user
+
 	validates_presence_of :name, :user
 end
