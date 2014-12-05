c8b5362143ca763c8d919dbb85057cd38c3111dd
rafaelrochasilva <rafaelrochasilva@gmail.com>
2014-12-05 17:14:57 -0200

    Add toggle to the controller

--- app/controllers/favorite_todos_controller.rb
+++ app/controllers/favorite_todos_controller.rb
@@ -4,4 +4,18 @@ class FavoriteTodosController < ApplicationController
 		@my_favorites = current_user.favorite_todos.all
 	end
 
+	def favorite
+		@todo_list = TodoList.find(params[:id])
+
+		if params[:type] == "favorite"
+			current_user.favorite_todos.create(todo_list_id: @todo_list.id)
+			flash[:notice] = "Add #{@todo_list.name} as favorite"
+			respond_with(@todo_list)
+		else
+			current_user.favorite_todos.delete(todo_list_id: @todo_list.id)
+			flash[:notice] = "Remove #{@todo_list.name} as favorite"
+			respond_with(nil, @todo_list)
+		end
+	end
+
 end
