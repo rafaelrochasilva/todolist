0526e9ab6c88c174eb363a80357e0ef5210735b4
rafaelrochasilva <rafaelrochasilva@gmail.com>
2014-12-08 14:03:16 -0200

    Change missing img file path

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -3,7 +3,7 @@ class TodoList < ActiveRecord::Base
 	has_many :list_items
 	has_many :favorite_todos
 
-	has_attached_file :photo, :styles => { :medium => "500x500>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :photo, :styles => { :medium => "500x500>", :thumb => "100x100>" }, :default_url => ":style/missing.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
 	validates_presence_of :name, :user
