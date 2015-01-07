5c32df9b9c99b0083bd60ea3fae9c660530b717f
rafaelrochasilva <rafaelrochasilva@gmail.com>
2015-01-07 14:59:32 -0200

    Create todolist_find method

--- app/controllers/list_item_controller.rb
+++ app/controllers/list_item_controller.rb
@@ -1,18 +1,16 @@
 class ListItemController < ApplicationController
 	before_action :authenticate_user!
+	before_action :find_todo_list
 
 	respond_to :json
 
 	def create
-		todo_list = TodoList.find(params[:todo_list_id])
-		if current_user.id == todo_list.user.id
-			@list_item = todo_list.list_items.create(list_item_params)
-		end
+		@list_item = @todo_list.list_items.create(list_item_params)
 		respond_with(@list_item, location: nil)
 	end
 
 	def destroy
-		@list_item = ListItem.find(params[:id])
+		@list_item = @todo_list.list_items.find(params[:id])
 		@list_item.destroy
 
 		respond_with(nil, location: todo_list_path(id: params[:todo_list_id]))
@@ -21,5 +19,9 @@ class ListItemController < ApplicationController
 	private
 	def list_item_params
 		params.require(:todo_list_item).permit(:description)
+	end
+
+	def find_todo_list
+		@todo_list = current_user.todo_lists.find(params[:todo_list_id])
 	end
 end
