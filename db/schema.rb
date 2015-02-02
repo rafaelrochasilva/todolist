378d9943d4fd03878095038af5a47efce19882d0
rafaelrochasilva <rafaelrochasilva@gmail.com>
2015-02-02 17:57:48 -0200

    Add feedentries table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141207184738) do
+ActiveRecord::Schema.define(version: 20150107170140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20141207184738) do
   create_table "favorite_todos", force: true do |t|
     t.integer "user_id"
     t.integer "todo_list_id"
+  end
+
+  create_table "feed_entries", force: true do |t|
+    t.integer  "user_id",    null: false
+    t.string   "message",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "list_items", force: true do |t|
