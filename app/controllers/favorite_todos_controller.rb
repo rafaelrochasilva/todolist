105e100228e2879874d7d2016030c93024ff8d44
rafaelrochasilva <rafaelrochasilva@gmail.com>
2015-02-02 17:59:26 -0200

    Save feed entry inside the favorite controller

--- app/controllers/favorite_todos_controller.rb
+++ app/controllers/favorite_todos_controller.rb
@@ -9,11 +9,17 @@ class FavoriteTodosController < ApplicationController
 
 		if params[:type] == "favorite"
 			favorite = current_user.favorite_todos.find_or_create_by(todo_list_id: @todo_list.id)
+			message = "Todo List #{@todo_list.name} was marked as favorite"
+			current_user.feed_entries.create(message: message)
 			flash[:notice] = "Add #{@todo_list.name} as favorite"
 		else
 			favorite = current_user.favorite_todos.find_by(todo_list_id: @todo_list.id)
 			if favorite
 				favorite.destroy
+
+				message = "Todo List #{@todo_list.name} was unmarked as favorite"
+				current_user.feed_entries.create(message: message)
+
 				flash[:notice] = "Remove #{@todo_list.name} as favorite"
 			end
 		end
