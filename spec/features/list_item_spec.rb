7428e8b7d54e80bacece9e46016c482e0b6bda3a
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-02-03 15:17:20 -0200

    Refactor list item failure messagesand organize the tests

--- spec/features/list_item_spec.rb
+++ spec/features/list_item_spec.rb
@@ -3,37 +3,26 @@ require 'rails_helper'
 feature 'ListItem management' do
 	let(:auth_user) { FactoryGirl.create(:auth_user) }
 	before { login_as(auth_user, scope: :user) }
-
 	let(:todo_list) { auth_user.todo_lists.create(name: "Todo Title")}
 
-
-	scenario "adds a new list item", :js => true do
+	scenario "User creates a new list item", :js => true do
+    list_item = todo_list.list_items.new(description: "text item")
 
 		visit todo_list_path(todo_list)
-
-		list_item = todo_list.list_items.new(description: "text item")
-
 		click_button '+ item'
-
 		fill_in 'description', with: list_item.description
-
 		click_button "Add"
 
 		expect(page).to have_content list_item.description
 	end
 
-	scenario "delete a list item", :js => true do
+	scenario "User deletes a list item", :js => true do
+    list_item = todo_list.list_items.new(description: "text item")
 
 		visit todo_list_path(todo_list)
-
-		list_item = todo_list.list_items.new(description: "text item")
-
 		click_button '+ item'
-
 		fill_in 'description', with: list_item.description
-
 		click_button "Add"
-
 		click_link "delete"
 
 		expect(page).to_not have_content list_item.description
