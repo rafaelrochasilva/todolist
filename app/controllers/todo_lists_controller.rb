ffae6ec0853d421fafd496a8f60ea57a98b37eaa
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-02-03 16:58:54 -0200

    Remove the find_todo_list to make the code more readable

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,6 +1,5 @@
 class TodoListsController < ApplicationController
 	before_action :authenticate_user!, except: [:index, :show]
-	before_action :find_todo_list, only: [:edit, :update, :destroy]
 
 	def index
 		@todo_lists = TodoList.public_visibility
@@ -25,24 +24,24 @@ class TodoListsController < ApplicationController
 	end
 
 	def edit
+    @todo_list = current_user.todo_lists.find(params[:id])
 	end
 
 	def update
+    @todo_list = current_user.todo_lists.find(params[:id])
 		@todo_list.update(todo_params)
 		respond_with(@todo_list)
 	end
 
 	def destroy
+    @todo_list = current_user.todo_lists.find(params[:id])
 		@todo_list.destroy
 		respond_with(@todo_list)
 	end
 
 	private
-	def todo_params
-		params.require(:todo_list).permit(:name, :private_todo, :user_id, :photo)
-	end
+  	def todo_params
+  		params.require(:todo_list).permit(:name, :private_todo, :user_id, :photo)
+  	end
 
-	def find_todo_list
-		@todo_list = current_user.todo_lists.find(params[:id])
-	end
 end
