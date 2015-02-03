f6373efc5c38c8b85dae2752b3bbfd962375c65f
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-02-03 14:53:35 -0200

    Refactor todo list feature displaying better error messages

--- spec/features/todo_list_spec.rb
+++ spec/features/todo_list_spec.rb
@@ -6,46 +6,40 @@ feature 'TodoList management' do
 
 	let(:todo_list) { user.todo_lists.create(name: "test") }
 
-
-	scenario 'adds a new todo list' do
-	  visit root_path
-	  click_link 'New'
-
-	  fill_in 'Name', with: todo_list.name
-
-	  click_button "Add"
-
-	  expect(page).to have_content 'Todo was successfully created.'
-	end
-
-	scenario 'displays all public todo lists' do
+	scenario 'User visit all public todo lists' do
 	  visit root_path
 	  click_link 'All'
 
 	  expect(page).to have_content "All Public Todos"
 	end
 
-	scenario 'display all private todo lists' do
+	scenario 'User visit all private todo lists' do
 		visit root_path
 		click_link "My Todos"
+
 		expect(page).to have_content "My Private Todos"
 	end
 
-	scenario 'display one todo list' do
-		visit todo_list_path(todo_list.id)
-		expect(page).to have_css ".card"
-	end
+  scenario 'User creates a new todo list' do
+    visit root_path
+    click_link 'New'
+    fill_in 'Name', with: todo_list.name
+    click_button "Add"
 
-	scenario 'update one todo list' do
+    expect(page).to have_content 'Todo list was successfully created.'
+  end
+
+	scenario 'User updates one todo list' do
 		visit todo_list_path(todo_list.id)
 		click_link "Update"
+
 		expect(page).to have_content "Edit Todo"
 	end
 
-	scenario 'delete one todo list' do
+	scenario 'User deletes one todo list' do
 		visit todo_list_path(todo_list.id)
 		click_link "X"
-		expect(page).to have_content "Todo Successfully destroyed."
-	end
 
+		expect(page).to have_content "Todo list was successfully destroyed."
+	end
 end
