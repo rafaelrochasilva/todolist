ae9c5a7561e1a323024415b1fad77d82e2d44797
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-02-04 10:00:18 -0200

    Remove list item object and let capybara create it

--- spec/features/list_item_spec.rb
+++ spec/features/list_item_spec.rb
@@ -6,11 +6,9 @@ feature 'A list items management' do
 	let(:todo_list) { auth_user.todo_lists.create(name: "Step by step to create a Monalisa's Graffiti")}
 
 	scenario "User creates a new list item", :js => true do
-    list_item = todo_list.list_items.new(description: "Create the background a little bit fuzzy, it will give an impression of depth.")
-
 		visit todo_list_path(todo_list)
 		click_button "+ item"
-		fill_in "description", with: list_item.description
+		fill_in "description", with: "Create the background a little bit fuzzy, it will give an impression of depth."
 		click_button "Add"
 
 		expect(page).to have_content "Create the background a little bit fuzzy, it will give an impression of depth."
