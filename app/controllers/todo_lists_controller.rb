ac0eca54eef2748c4255509d31896b0f5c300803
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-02-05 13:43:36 -0200

    Fix indentation problem

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,51 +1,50 @@
 class TodoListsController < ApplicationController
-	before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 
-	def index
-		@todo_lists = TodoList.public_visibility
-	end
+  def index
+    @todo_lists = TodoList.public_visibility
+  end
 
-	def my_todos
-		@my_todos = current_user.todo_lists.all
-	end
+  def my_todos
+    @my_todos = current_user.todo_lists.all
+  end
 
-	def show
+  def show
     if user_signed_in?
       @todo_list = current_user.todo_lists.find(params[:id])
     else
       @todo_list = TodoList.public_visibility.find(params[:id])
     end
-		@favorite = @todo_list.favorite_todos.find_by(user_id: current_user)
-	end
+    @favorite = @todo_list.favorite_todos.find_by(user_id: current_user)
+  end
 
-	def new
-		@todo_list = TodoList.new
-	end
+  def new
+    @todo_list = TodoList.new
+  end
 
-	def create
-		@todo_list = current_user.todo_lists.create(todo_params)
-		respond_with(@todo_list)
-	end
+  def create
+    @todo_list = current_user.todo_lists.create(todo_params)
+    respond_with(@todo_list)
+  end
 
-	def edit
-		@todo_list = current_user.todo_lists.find(params[:id])
-	end
+  def edit
+    @todo_list = current_user.todo_lists.find(params[:id])
+  end
 
-	def update
-		@todo_list = current_user.todo_lists.find(params[:id])
-		@todo_list.update(todo_params)
-		respond_with(@todo_list)
-	end
+  def update
+    @todo_list = current_user.todo_lists.find(params[:id])
+    @todo_list.update(todo_params)
+    respond_with(@todo_list)
+  end
 
-	def destroy
-		@todo_list = current_user.todo_lists.find(params[:id])
-		@todo_list.destroy
-		respond_with(@todo_list)
-	end
+  def destroy
+    @todo_list = current_user.todo_lists.find(params[:id])
+    @todo_list.destroy
+    respond_with(@todo_list)
+  end
 
-	private
-	def todo_params
-		params.require(:todo_list).permit(:name, :private_todo, :user_id, :photo)
-	end
-
+  private
+  def todo_params
+    params.require(:todo_list).permit(:name, :private_todo, :user_id, :photo)
+  end
 end
