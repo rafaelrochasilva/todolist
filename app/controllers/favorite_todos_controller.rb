8264a24137ce8e8e94f01e04595ef1e99e0b9970
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-02-05 13:59:14 -0200

    Fix merge conflict into favorite todo

--- app/controllers/favorite_todos_controller.rb
+++ app/controllers/favorite_todos_controller.rb
@@ -4,35 +4,23 @@ class FavoriteTodosController < ApplicationController
     @my_favorites = current_user.favorite_todos.includes(:todo_list).all
   end
 
-  def favorite
-    @todo_list = TodoList.find(params[:id])
+  def create
+    todo_list = TodoList.find(params[:id])
+    favorite = current_user.favorite_todos.create(todo_list_id: todo_list.id)
 
-    if params[:type] == "favorite"
-      favorite = current_user.favorite_todos.find_or_create_by(todo_list_id: @todo_list.id)
-      message = "Todo List #{@todo_list.name} was marked as favorite"
-      current_user.feed_entries.create(message: message)
-      flash[:notice] = "Add #{@todo_list.name} as favorite"
-    else
-      favorite = current_user.favorite_todos.find_by(todo_list_id: @todo_list.id)
-      if favorite
-        favorite.destroy
+    current_user.feed_entries.create(message: "Todo List #{todo_list.name} was marked as favorite")
 
-        message = "Todo List #{@todo_list.name} was unmarked as favorite"
-        current_user.feed_entries.create(message: message)
-
-        flash[:notice] = "Remove #{@todo_list.name} as favorite"
-      end
-    end
-    respond_with(nil, @todo_list)
+    respond_with(todo_list)
   end
 
-	def create
-		todo_list = TodoList.find(params[:id])
-		favorite = current_user.favorite_todos.find_or_create_by(todo_list_id: todo_list.id)
+  def destroy
+    todo_list = TodoList.find(params[:id])
+    
+    favorite = current_user.favorite_todos.find_by(todo_list_id: todo_list.id)
+    favorite.delete
 
-		current_user.feed_entries.create(message: "Todo List #{todo_list.name} was marked as favorite")
-		flash[:notice] = "Add #{todo_list.name} as favorite"
+    current_user.feed_entries.create(message: "Todo List #{todo_list.name} was unmarked as favorite")
 
-		respond_with(todo_list)
-	end
+    respond_with(todo_list)
+  end
 end
